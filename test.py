d19feacab978a4026aaedc95beb494ff2bc73fd2
Dane Henry <dhenry@lawv.net>
2025-03-14 09:32:45 -0400

    updating

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-import random
+def namespace_test():
+    x = 42
 
-print(random.random())
+    print("Inner X:",id(x))
+    print("Inner Int:",id(42))
+
+x = 27
+
+print("Outer X:",id(x))
+print("Outer Int:",id(27))
+namespace_test()
