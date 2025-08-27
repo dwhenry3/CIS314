627fa6af43ae0bb349d1ff506f4c0aa4827f7f17
Dane Henry <dhenry@lawv.net>
2025-08-27 16:31:23 -0400

    Initial Commit

---
+++ test.py
@@ -0,0 +1,25 @@
+''''''
+print("Hello World")
+'''
+x = 3
+if x == 7:
+  print("x is 7!")
+elif x >= 14:
+  print("x is greater than or equal to 14")
+else:
+  print("x is something else")
+
+x = 1
+while x < 7:
+  print(x)
+  x += 1
+
+counties = ["Berkeley","Jefferson","Morgan"]
+for x in counties:
+  print(x)
+
+def printHello():
+  print("Hello World")
+
+printHello()
+'''
